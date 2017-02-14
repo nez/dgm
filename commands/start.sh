ecfd8c7a878e11fd9046cc8d8a0aa16e58394a30
Andres Gomez Urquiza <andres.gomez@datos.mx>
2017-02-13 22:33:11 -0600

    dont run 'node user.js'

--- commands/start.sh
+++ commands/start.sh
@@ -4,5 +4,5 @@ rm -f config/db.js
 rm -rf public/tmp
 ./commands/replace_env.sh config/app.example.js config/app.js
 ./commands/replace_env.sh config/db.example.js config/db.js
-node user.js
+#node user.js
 npm start
